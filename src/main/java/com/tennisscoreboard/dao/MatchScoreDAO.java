129e12013859162053c5e1d27284c9ab23a908be
immagixe <immagixe@gmail.com>
2022-07-23 16:43:27 +0300

    Change MarchScoreDAO

--- src/main/java/com/tennisscoreboard/dao/MatchScoreDAO.java
+++ src/main/java/com/tennisscoreboard/dao/MatchScoreDAO.java
@@ -1,5 +1,6 @@
 package com.tennisscoreboard.dao;
 
+import com.tennisscoreboard.models.Match;
 import com.tennisscoreboard.models.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -33,22 +34,21 @@ public class MatchScoreDAO {
             return player;
         }
     }
+    @Transactional(readOnly = true)
+    public Player getPlayerById(int playerId) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Player.class, playerId);
+    }
 
-//    @Transactional
-//    public Player isPlayerAlreadyCreated(Player player) {
-//        Session session = sessionFactory.getCurrentSession();
-//        String playerName = player.getName();
-//
-//        String hql = "FROM Player WHERE name = :name";
-//        List<Player> playerList = session.createQuery(hql).setParameter("name", playerName).getResultList();
-//        int countPlayers = playerList.size();
-//
-//        if (countPlayers != 0) {
-//            return playerList.get(0);
-//        } else {
-//            session.save(player);
-//            return player;
-//        }
-//    }
+    @Transactional
+    public void setWinnerOfMatch(Match match, Player player) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(match);
+    }
+
+    @Transactional
+    public void saveMatch(Match match) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(match);
+    }
 }
-
